d737c132565ab243febce6998acb1132b9bdf04d
Alex Shyba <as@altola.co>
2020-04-25 13:32:00 -0700

    cosmetic: switching to RichText field helpers

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, Link } from '@sitecore-jss/sitecore-jss-react';
+import { Text, RichText, Link } from '@sitecore-jss/sitecore-jss-react';
 import Logo from '../Logo';
 import NavLinks from '../NavLinks';
 
@@ -175,10 +175,10 @@ const Hero = ({ fields }) => {
             <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
               <Text field={title} />
               <br className="xl:hidden" />{' '}
-              <Text field={subtitle} tag="span" className="text-red-500" />
+              <RichText field={subtitle} tag="span" className="text-red-500" />
             </h2>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-              <Text field={text} />
+              <RichText tag="span" field={text} />
             </p>
             <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
               {primaryCTATitle &&
