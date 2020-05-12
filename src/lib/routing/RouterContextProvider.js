13ce622e235d28e3784d38f27e3753fc2fac6227
Adam Weber <232242+aweber1@users.noreply.github.com>
2020-05-12 08:06:07 -0500

    Updated dynamic routing code to mirror the same router/route API that Next.js exposes via `useRouter`.

--- src/lib/routing/RouterContextProvider.js
+++ src/lib/routing/RouterContextProvider.js
@@ -1,32 +1,13 @@
 import React from 'react';
-import Router from 'next/router';
-import { routeMatcher } from './routeMatcher';
-
-function wrapRouterMethod(method) {
-  return (url, as, options) => {
-    const { route, params } = Router.matchRoute(url);
-
-    const resolvedHref = route ? route.getHref(params) : href;
-    // TODO: review. Is this correct logic for resolving `as`?
-    const resolvedAs = as || href;
-
-    return Router[method](resolvedHref, resolvedAs, options);
-  };
-}
-
-// This concept borrowed from `next-routes` library.
-Router.pushRoute = wrapRouterMethod('push');
-Router.replaceRoute = wrapRouterMethod('replace');
-Router.prefetchRoute = wrapRouterMethod('prefetch');
-
-Router.matchRoute = function matchRoute(url) {
-  const matched = routeMatcher(url, Router.routeDefinitions);
-  return matched;
-};
+import { useRouter } from 'next/router';
+import { enhanceRouter } from './Router';
 
 export const RouterContext = React.createContext(null);
 
 export default ({ children, routeDefinitions }) => {
-  Router.routeDefinitions = routeDefinitions;
-  return <RouterContext.Provider value={{ router: Router }}>{children}</RouterContext.Provider>;
+  const nextRouter = useRouter();
+  const enhancedRouter = enhanceRouter(nextRouter, routeDefinitions);
+  return (
+    <RouterContext.Provider value={{ router: enhancedRouter }}>{children}</RouterContext.Provider>
+  );
 };
