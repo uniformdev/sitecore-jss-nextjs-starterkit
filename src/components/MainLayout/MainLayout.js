8e5a99b5f0fe879e1571598c67949c40175c5e8a
Alex Shyba <alex.shyba@uniform.dev>
2021-06-29 18:29:09 -0700

    cleanup

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -1,19 +1,14 @@
-import { Placeholder } from '@sitecore-jss/sitecore-jss-react';
-import { getBoolEnv } from '@uniformdev/common';
-import { EsiPlaceholder } from '@uniformdev/esi-jss-react';
-import styles from './MainLayout.module.css';
-import Footer from '../Footer/Footer';
-
-const esiEnabled = getBoolEnv(process.env, 'UNIFORM_OPTIONS_ESI', false);
+import { Placeholder } from "@sitecore-jss/sitecore-jss-react";
+import styles from "./MainLayout.module.css";
+import Footer from "../Footer/Footer";
 
 const MainLayout = ({ route }) => (
-    <div className={styles.container}>
-        {esiEnabled && <EsiPlaceholder rendering={route} />}
-        <main className={styles.main}>
-            <Placeholder name="uniform-jss-kit-content" rendering={route} />
-        </main>
-        <Footer />
-    </div>
+  <div className={styles.container}>
+    <main className={styles.main}>
+      <Placeholder name="uniform-jss-kit-content" rendering={route} />
+    </main>
+    <Footer />
+  </div>
 );
 
 export default MainLayout;
